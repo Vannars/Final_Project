0d2235d86708b26b00cdd93a92c544b31e856c1d
Vannars <151542822+Vannars@users.noreply.github.com>
2025-03-06 12:53:54 +0000

    Extension 1: Extract Sentence + addition tweaks
    Logs:
    -added get_sentences to extract sentences from the text using the sentencizer class in spaCY.
    
    - added test case for running get_sentences
    
    -tweaked max_length to allow longer text processing

--- src/python/components.py
+++ src/python/components.py
@@ -2,24 +2,27 @@ from transformers import AutoModelWithLMHead, AutoTokenizer
 import spacy
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm") # loads the tokenizer for english
 
-tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
-model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
+tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap") # a tokenizer is a class used for converting strings to lists of tkens
+model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap") 
 
-def get_question(answer, context, max_length=64):
-    input_text = "answer: %s  context: %s </s>" % (answer, context)
-    features = tokenizer([input_text], return_tensors='pt')
+def get_question(answer, context, max_length=250):
+    input_text = "answer: %s  context: %s </s>" % (answer, context) # string template (answer, context replaces %s)
+    features = tokenizer([input_text], return_tensors='pt') 
 
     output = model.generate(input_ids=features['input_ids'], 
-                            attention_mask=features['attention_mask'],
-                            max_length=max_length)
+                            attention_mask=features['attention_mask'], # attention mask - pads inputs to the same length
+                            max_length=max_length) # max length is max_length defined in tha parameter
+                            
 
-    return tokenizer.decode(output[0])
+    return tokenizer.decode(output[0]) # output a string from the first element of the output array
 
 context = "Manuel has created RuPERTa-base with the support of HF-Transformers and Google"
 answer = "Manuel"
 
+
+# Original version ===================================={
 question = get_question(answer, context)
 print("Generated question:", question)
 
@@ -27,9 +30,21 @@ print("Generated question:", question)
 doc = nlp(context)
 for token in doc:
     print(token.text, token.pos_, token.dep_)
-
 # output: question: Who created the RuPERTa-base?
+#Original version =====================================}
 
+def get_sentences(text): 
+    doc = nlp(text) #ref link: https://spacy.io/api/doc
+    sentences = list(doc.sent) # sent comes from the sentencizer class https://spacy.io/api/sentencizer (iterates from the default boundary which is a period ".")
+    return [s.text for s in sentences] # returning sentences for all elements that have a .text attribute (hopefully all of them) 
+#Test
+if __name__ == "__main__": # ref link: https://www.geeksforgeeks.org/what-does-the-if-__name__-__main__-do/)
+    text = "The PlayStation 5 (PS5) is Sony's latest gaming console. It features a custom SSD for fast loading."
+    sentence = get_sentences(text)
+    print(sentence)
+    #output: ['The PlayStation 5 (PS5) is Sony's latest gaming console.', 'It features a custom SSD for fast loading.'] Successfull
+
+# CITATION
 
 #THIS IS THE CITATION FOR THE T5 MODEL USED IN THE QUESTION GENERATION COMPONENT
 # THIS CODE HAS BEEN HEAVIL MODIFIED IN ORDER TO FIT THE NEEDS OF THE PROJECT
